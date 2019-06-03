e0463c58cf218e72d318564cb0566fb0a5249bc8
patcrowe <patcrowe@umich.edu>
2019-06-03 17:09:04 -0400

    Small fixes and additional function

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 import random
-import BlackJackClass
+
 #Begin Card Class
 class Card:
 	def __init__(self,suit,value):
@@ -24,13 +24,14 @@ class Card:
 #End Card Class
 
 class Player:
-	def __init__(self, p_type, name = "", hand = None, chips = 1000, winnings = 0):
+	def __init__(self, p_type, name = "", hand = None, chips = 1000, winnings = 0, total_val = 0):
 		self.p_type = p_type
 		self.chips = chips
 		if hand is None:
 			self.hand = []
 		self.winnings = winnings
 		self.name = name
+		self.total_val = total_val
 
 	def __repr__(self):
 		out = ""
@@ -48,7 +49,7 @@ class Player:
 
 def init_deck(deck):
 	deck = []
-	suits = ["hearts", "clubs", "diamonds", "spades"]
+	suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
 	for x in suits:
 		for y in range(1,14,1):
 			deck.append(Card(x,y))
@@ -62,21 +63,41 @@ def shuffle(deck):
 
 def deal(player, dealer, deck, counter):
 	for x in range(2):
+		if counter >= 52:
+			counter = 0
+			shuffle(deck)
 		player.hand.append(deck[counter])
+		player.total_val += deck[counter].value
 		counter += 1
+		if counter >= 52:
+			counter = 0
+			shuffle(deck)
 		dealer.hand.append(deck[counter])
+		dealer.total_val += deck[counter].value
 		counter += 1
 
 
 def clear_hands(player, dealer):
 	player.hand = []
 	dealer.hand = []
+	player.total_val = 0
+	dealer.total_val = 0
 
 
 def hit(player, deck, counter):
+	if counter >= 52:
+		counter = 0
+		shuffle(deck)
 	player.hand.append(deck[counter])
+	player.total_val += deck[counter].value
 	print("{} hits, {}".format(player.name, deck[counter]))
 	counter += 1
+
+def play_hand(player, deck, counter):
+	x = input("Enter H to hit, or S to stay: ")
+	while x.upper() == "H":
+		hit(player, deck, counter)
+
 
 deck = []
 deck = init_deck(deck)
@@ -88,11 +109,13 @@ dealer = Player(p_type = "D",chips = 1000000)
 player = Player(p_type = "H",name = "Player1")
 
 while playing:
-	if counter >= 50:
-		counter = 0
-		shuffle(deck)
 
 	deal(player, dealer, deck, counter)
 	print(player)
 	print(dealer)
+
+
+
+
+
 	playing = False
